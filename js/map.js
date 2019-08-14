b874e2c85cb75759785d180ecb263be2475e1c32
LucasAguirre1986 <lucas.aguirre1986@gmail.com>
2019-08-13 21:50:42 -0300

    Creacion de formulario

--- js/map.js
+++ js/map.js
@@ -1,43 +1,34 @@
 // Setea el map inicial
-var map = L.map('map').setView([-34.555990, -58.474466], 13);
+var map = L.map('map').setView([-34.555990, -58.474466], 14);
 
 L.tileLayer(
     'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 20, // Setea el zoom maximo 
     }).addTo(map); // Define a map => Div del html
 
-// Recorre cada uno de los puntos del objeto "ListaDePuntos"
-// ListaDePuntos se encuentra en el script map
-ListaDePuntos.forEach(punto => {
-    // Construye el card por cada punto
-    let card = `
-    <div class="tab-content" id="myTabContent">
-        <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-        <img src="https://www.pcactual.com/medio/2017/06/30/google-maps_51b78e0b.jpg" class="imgPopUp"/>
-        <h5>${punto.nombre}</h5>
-        <strong>Dirección: </strong>${punto.direccion}<br>
-        <strong>Categoria: </strong>${punto.categoria}<br>
-        <strong>Teléfono: </strong>${punto.telefono}<br>
-        <strong>Coordenadas: </strong>${punto.coordenada[0]}, ${punto.coordenada[1]}<br>
-        </div>
-    </div>`;
-
-    // Crea los puntos en el mapa
-    marker = new L.marker([
-        punto.coordenada[0], // Latitud 
-        punto.coordenada[1] // Longitud
-    ])
-        .bindPopup(card) // Card => Armado de estructura para mostrar
-        .addTo(map);
-});
+// Ejecuta la funcion para crear los marcadores en el mapa
+crearMarcadoresMap(ListaDePuntos)
 
 var popup = L.popup();
 function onMapClick(e) {
-    $('#modalAgregar').modal('show'); 
-    popup
-        .setLatLng(e.latlng)
-        .setContent("You clicked the map at " + e.latlng.toString())
-        .openOn(map);
+    // Limpiar los inputs
+    nombreForm.value = "";
+    direccionForm.value = "";
+    telefonoForm.value = "";
+    categoriaForm.value = "";
+    latitudForm.textContent = "";
+    longitudForm.textContent = "";
+
+    $('#modalAgregar').modal('show'); // Activa el modal
+    activarTabs(1) // Se posiciona en el primer tab
+
+    // busca la direccion => Funcion en modal.js
+    buscarDireccionXCoordenada(e.latlng.lat, e.latlng.lng)
+        .then((data)=>{
+            direccionForm.value = data.display_name;
+            latitudForm.textContent = e.latlng.lat;
+            longitudForm.textContent = e.latlng.lng;
+        })
 }
 // Evento al presionar click en el mapa
 map.on('click', onMapClick);
@@ -47,4 +38,32 @@ L.circle([-34.555990, -58.474466], 1100, {
     fillColor: '#f03',
     fillOpacity: 0.2
 }).addTo(map);
-     //.bindPopup("I am a circle."); para agregar un comentario
+//.bindPopup("I am a circle."); para agregar un comentario
+
+
+// Recorre cada uno de los puntos del objeto "ListaDePuntos"
+function crearMarcadoresMap(ListaDePuntos) {
+    // ListaDePuntos se encuentra en el script map
+    ListaDePuntos.forEach(punto => {
+        // Construye el card por cada punto
+        let card = `
+        <div class="tab-content" id="myTabContent">
+            <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+            <img src="https://www.pcactual.com/medio/2017/06/30/google-maps_51b78e0b.jpg" class="imgPopUp"/>
+            <h5>${punto.nombre}</h5>
+            <strong>Dirección: </strong><span class="nombrePropio">${punto.direccion}</span><br><hr>
+            <strong>Categoria: </strong>${punto.categoria}<br>
+            <strong>Teléfono: </strong>${punto.telefono}<br>
+            <strong>Coordenadas: </strong>${Math.round(punto.coordenada[0] * 1000) / 1000}, ${Math.round(punto.coordenada[1] * 1000) / 1000}<br>
+            </div>
+        </div>`;
+
+        // Crea los puntos en el mapa
+        marker = new L.marker([
+            punto.coordenada[0], // Latitud 
+            punto.coordenada[1] // Longitud
+        ])
+            .bindPopup(card) // Card => Armado de estructura para mostrar
+            .addTo(map);
+    });
+}
--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,128 @@
 // Controla / Manipula el dom del modal
-let btnSiguiente = document.querySelector('#siguiente')
+let btnAgregar = document.querySelector('#nuevo'); //Boton de modal "Agregar nuevo"
+let btnSiguiente = document.querySelector('#siguiente'); // Botones de siguiente
+let btnAnterior = document.querySelector('#anterior'); // Botones de anterior
 
-btnSiguiente.addEventListener('click',function(){
-    $('.nav-tabs a[href="#profile"]').click();
+// Tabs
+let tab1 = document.querySelector('#paso1-tab')
+let tab2 = document.querySelector('#paso2-tab')
+let tabPaso1 = document.querySelector('#paso1')
+let tabPaso2 = document.querySelector('#paso2')
+
+// Input de form
+let nombreForm = document.querySelector('#nombre')
+let telefonoForm = document.querySelector('#telefono')
+let categoriaForm = document.querySelector('#categoria')
+let direccionForm = document.querySelector('#direccion')
+let latitudForm = document.querySelector('#latitud')
+let longitudForm = document.querySelector('#longitud')
+let agregarForm = document.querySelector('#agregar')
+let btnBuscar = document.querySelector('#buscar')
+
+// TABS ===============================================================
+function activarTabs(numeroTab){
+    if(numeroTab==1){
+        tabPaso1.classList.add('show')
+        tabPaso1.classList.add('active')
+        tabPaso2.classList.remove('show')
+        tabPaso2.classList.remove('active')
+        tab1.classList.add('active')
+        tab2.classList.remove('active')
+    }else{
+        tabPaso1.classList.remove('show')
+        tabPaso1.classList.remove('active')
+        tabPaso2.classList.add('show')
+        tabPaso2.classList.add('active')
+        tab1.classList.remove('active')
+        tab2.classList.add('active')
+    }
+}
+
+
+// MODAL ==============================================================
+btnAgregar.addEventListener('click', function () {
+
+    // Limpiar los inputs
+    nombreForm.value = "";
+    direccionForm.value = "";
+    telefonoForm.value = "";
+    categoriaForm.value = "";
+    latitudForm.textContent = "";
+    longitudForm.textContent = "";
+
+    $('#modalAgregar').modal('show'); // Activa el modal
+    $('.nav-tabs a[href="#home"]').click(); // Se posiciona en el primer tab
+    // Asigna por defecto el tab 1
+    activarTabs(1)
+
 })
+
+btnSiguiente.addEventListener('click', function () {
+    activarTabs(2)
+})
+
+btnAnterior.addEventListener('click', function () {
+    activarTabs(1)
+})
+
+// FORM ==============================================================
+agregarForm.addEventListener('click', function () {
+
+    ListaDePuntos.push(
+        {
+            nombre: (nombreForm.value).toUpperCase(),
+            direccion: direccionForm.value,
+            telefono: telefonoForm.value,
+            categoria: categoriaForm.value,
+            coordenada: [Number(latitudForm.textContent), Number(longitudForm.textContent)]
+        }
+    )
+
+    // Ejecuta la funcion para crear los marcadores en el mapa
+    crearMarcadoresMap(ListaDePuntos)
+})
+
+// BUSCAR COORDENADAS ==============================================================
+btnBuscar.addEventListener('click', function () {
+    fetch(`https://nominatim.openstreetmap.org/search?format=json&limit=15&q=${direccionForm.value}`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (obj) {
+            document.getElementById("listaResultado").innerHTML = "" // Limpia los LI
+
+            for (var i = 0; i < obj.length; i++) {
+                let ul = document.getElementById("listaResultado"); // Selecciona la ul                
+                let li = document.createElement('li'); // Crea un li
+                li.setAttribute('class', 'list-group-item'); // Asigna la clase
+                li.setAttribute('data-lat', obj[i].lat); // Asigna un atributo id al li para saber la latitud.
+                li.setAttribute('data-lng', obj[i].lon); // Asigna un atributo id al li para saber la longitud.
+                li.appendChild(document.createTextNode(obj[i].display_name)); // Asigna el nombre de la calle
+                ul.appendChild(li);
+            }
+        })
+        .then(() => {
+            // Agrega evento a todos los li para que al presionarlo agregue la direccion y coordenadas
+            let li = document.querySelectorAll('#listaResultado li')
+            for (var i = 0; i < li.length; i++) {
+                li[i].addEventListener('click', function () {
+                    direccionForm.value = this.textContent;  // Seteo el valor del input
+                    latitudForm.textContent = this.getAttribute('data-lat'); // Seteo el valor del input
+                    longitudForm.textContent = this.getAttribute('data-lng'); // Seteo el valor del input
+                })
+            }
+        });
+})
+
+
+// Funcion para buscar la direccion a partir de las coordenadas
+function buscarDireccionXCoordenada(lat, long) {
+    // busca la direccion en funcion de la direccion
+    return fetch(`https://nominatim.openstreetmap.org/reverse.php?lat=${lat}&lon=${long}&format=json`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (obj) {
+            return obj // Retorna el valor encontrado
+        });
+}
